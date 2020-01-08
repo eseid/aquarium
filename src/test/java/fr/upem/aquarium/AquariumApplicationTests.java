cd5ca2b76e105a79bb2df5923600f2c1ba5294ee
Eseid BENMAMMAR <e.benmammar@everteam-gs.com>
2020-01-08 20:35:43 +0100

    initial commit

---
+++ src/test/java/fr/upem/aquarium/AquariumApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upem.aquarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
